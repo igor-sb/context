c05d0a4e04bf2c44e821c4104138be759c1a57c4
igor-sb <igor.segota@sapient.bio>
2022-12-17 23:10:43 +0100

    feat: switch to non-standard evaluation with as

--- R/context.R
+++ R/context.R
@@ -12,7 +12,6 @@ create_context_manager <- function(
     on_enter = function() {},
     on_exit = function() {},
     on_error = NULL,
-    as = NULL,
     args = NULL
 ) {
   structure(
@@ -20,7 +19,6 @@ create_context_manager <- function(
       on_enter = on_enter,
       on_exit = on_exit,
       on_error = on_error,
-      as = as,
       args = args
     ),
     class = "ContextManager"
@@ -29,21 +27,45 @@ create_context_manager <- function(
 
 #' Context manager's `with` function
 #'
-#' @param context [`ContextManager`] object
-#' @param expression Expression or code block to be evaluated within the context
+#' @param data [`ContextAs`] S3 object with elements `context` and `variable`
+#' @param expr Expression or code block to be evaluated within the context
+#' @param ... Unused. Here for consistency with `base::with()`.
 #'
 #' @export
-with <- function(context, expression) {
+with.ContextAs <- function(data, expr, ...) {
+  keyword_args <- list(...)
+  eval_environment <- if ("environment" %in% names(keyword_args)) {
+    keyword_args$environment
+  } else {
+    parent.frame()
+  }
+  context <- data$context
+  as_variable <- data$variable
   tryCatch({
     enter_result <- get_enter_result(context$on_enter, context$args)
-    set_variable_in_environment(context$as, enter_result, parent.frame())
-    eval(expression, parent.frame())
+    set_variable_in_environment(as_variable, enter_result, eval_environment)
+    eval(expr, eval_environment)
   },
   error = context$on_error,
   finally = {
     context$on_exit(enter_result)
-    remove_variable_from_environment(context$as, parent.frame())
+    remove_variable_from_environment(as_variable, eval_environment)
   })
+}
+
+# This won't work. This will evaluate expr in each list element separately
+with.list <- function(data, expr, ...) {
+  # Run tryCatch by vectorizing set_variable_in_environment
+  #  get_enver_result
+  #  remove_variable_from_environment
+  #
+  calling_environment <- parent.frame()
+  invisible(lapply(
+    data,
+    with.ContextAs,
+    expr = expr,
+    environment = calling_environment
+  ))
 }
 
 get_enter_result <- function(on_enter_fun, on_enter_args) {
@@ -62,6 +84,25 @@ remove_variable_from_environment <- function(variable, environment) {
   }
 }
 
+#' Context manager's `as` function
+#'
+#' @param context Context manager constructor.
+#' @param variable Variable name to use within the context.
+#'
+#' @return List with elements `context` and `variable`.
+#' @export
+`%as%` <- function(context, variables) {
+  variables_symbols <- rlang::ensyms(variables)
+  variables_strings <- sapply(variables_symbols, rlang::as_string)
+  structure(
+    list(
+      context = context,
+      variable = variables_strings
+    ),
+    class = "ContextAs"
+  )
+}
+
 #' Open file implementation using a context manager
 #'
 #' Similar to Python's `open()`, intended to be used with a context manager.
@@ -72,12 +113,13 @@ remove_variable_from_environment <- function(variable, environment) {
 #'
 #' @return S3 class ContextManager
 #' @export
-open_file <- function(file_name, mode, as) {
+open_file <- function(file_name, mode) {
   create_context_manager(
     on_enter = function(file_name, mode) file(file_name, open = mode),
     on_exit = function(file_connection) close(file_connection),
     on_error = NULL,
-    as = as,
     args = list(file_name = file_name, mode = mode)
   )
 }
+
+# Multiple: Vectorize open_file and %as% ?
--- tests/testthat/test-context.R
+++ tests/testthat/test-context.R
@@ -1,7 +1,21 @@
 test_that("open and read file line by line:", {
+  set.seed(42)
   temp_file <- tempfile()
   writeLines(LETTERS[1:5], temp_file)
-  with(open_file(temp_file, "r", "f"), {
+  with(open_file(temp_file, "r") %as% f, {
+    for (i in 1:5) {
+      line <- readLines(f, n = 1)
+      expect_identical(line, LETTERS[i])
+    }
+  })
+  invisible(file.remove(temp_file))
+})
+
+test_that("open and read file line by line, list context:", {
+  set.seed(42)
+  temp_file <- tempfile()
+  writeLines(LETTERS[1:5], temp_file)
+  with(list(open_file(temp_file, "r") %as% f), {
     for (i in 1:5) {
       line <- readLines(f, n = 1)
       expect_identical(line, LETTERS[i])
@@ -11,22 +25,35 @@ test_that("open and read file line by line:", {
 })
 
 test_that("open file and catch an error:", {
-  open_file_catch <- function(file_name, mode, as) {
+  open_file_catch <- function(file_name, mode) {
     create_context_manager(
       on_enter = function(file_name, mode) file(file_name, open = mode),
       on_exit = function(file_connection) close(file_connection),
-      on_error = function(error) stop("Cought an error"),
-      as = as,
+      on_error = function(error) stop("Caught an error"),
       args = list(file_name = file_name, mode = mode)
     )
   }
   temp_file <- tempfile()
   writeLines(LETTERS[1:5], temp_file)
   expect_error(
-    with(open_file_catch(temp_file, "r", "f"), {
+    with(open_file_catch(temp_file, "r") %as% f, {
       stop()
     }),
-    "Cought an error"
+    "Caught an error"
   )
   invisible(file.remove(temp_file))
+})
+
+test_that("open and read file line by line (multiple)", {
+  set.seed(42)
+  file1 <- tempfile()
+  file2 <- tempfile()
+  writeLines(LETTERS[1:5], file1)
+  writeLines(LETTERS[6:10], file2)
+  with(list(open_file(file1, "r") %as% f1, open_file(file2, "r") %as% f2), {
+    for (i in 1:5) {
+      cat(readLines(f1, n = 1), " <-> ", readLines(f2, n = 1))
+    }
+  })
+  invisible(file.remove(file1, file2))
 })
